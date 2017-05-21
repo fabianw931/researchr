bae4960c92df202205a39cbae3fbd5059c61d563
Fabian Wildhaber <fabianwildhaber@gmail.com>
2017-05-21 15:49:27 +0200

    add new gui structure

--- src/ch/fhnw/researchr/Main.java
+++ src/ch/fhnw/researchr/Main.java
@@ -1,19 +1,21 @@
 package ch.fhnw.researchr;
 
-import ch.fhnw.researchr.controller.ResearchrController;
 import ch.fhnw.researchr.model.ResearchrModel;
 import ch.fhnw.researchr.view.ResearchrView;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     //private static String UI = "resources/view/UI.fxml";
 
+    /*
     private ResearchrModel model;
     private ResearchrView view;
     private ResearchrController controller;
+    */
 
     public static void main(String[] args) {
         launch(args);
@@ -27,16 +29,39 @@ public class Main extends Application{
         primaryStage.show();
          */
 
+        /*
         model = new ResearchrModel();
         view = new ResearchrView(primaryStage, model);
         controller = new ResearchrController(view, model);
         view.start();
+        */
+        try{
 
+            ResearchrModel model = new ResearchrModel();
+
+            BorderPane rootPanel = new ResearchrView(model);
+
+            Scene scene = new Scene(rootPanel, 300, 300);
+
+            String stylesheet = getClass().getResource("resources/css/style.css").toExternalForm();
+            scene.getStylesheets().add(stylesheet);
+
+            primaryStage.titleProperty().bind(model.windowTitleProperty());
+            primaryStage.setScene(scene);
+
+            primaryStage.show();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getCause());
+        }
     }
 
     @Override
     public void stop(){
-        if (view != null) view.stop();
+        System.exit(0);
+        //if (view != null) view.stop();
     }
 
 }
--- src/ch/fhnw/researchr/view/ResearchrView.java
+++ src/ch/fhnw/researchr/view/ResearchrView.java
@@ -1,27 +1,73 @@
 package ch.fhnw.researchr.view;
 
 import ch.fhnw.researchr.model.ResearchrModel;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
-import java.awt.*;
+public class ResearchrView extends BorderPane{
 
-public class ResearchrView extends BorderLayout{
 
+    /*
     final private Stage stage;
-    final private ResearchrModel model;
 
     public ResearchrView(Stage stage, ResearchrModel model){
         this.stage = stage;
         this.model = model;
-    }
 
+    }
     public void start() {
         stage.show();
     }
 
     public void stop() {
         stage.hide();
+    }
+
+    */
+    final private ResearchrModel model;
+
+    private Button          button;
+    private ListView<Color> listView;
+
+    public ResearchrView(ResearchrModel model) {
+        this.model = model;
+
+        try {
+
+            initializeControls();
+            layoutControls();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    private void initializeControls() {
+        initializeMenuBar();
+        initializeSidePanel();
+        initializePropertiesView();
+    }
+
+    private void initializeMenuBar(){
+        Image newBtn = new Image(getClass().getResourceAsStream("resources/img/add.png"));
+        button = new Button("This is cool", new ImageView(newBtn));
+    }
+
+    private void initializeSidePanel() {
+    }
+
+    private void initializePropertiesView() {
+    }
+
+    private void layoutControls() {
+
+        ToolBar toolbar = new ToolBar(button);
+        setTop(toolbar);
     }
 }
